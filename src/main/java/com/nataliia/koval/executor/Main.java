a0e112187ae4bb9499108a4664b71a778668ce13
Nataliia <ruba.fish1500@gmail.com>
2024-07-08 18:48:26 +0300

    change MyFixedThreadPool

--- src/main/java/com/nataliia/koval/executor/Main.java
+++ src/main/java/com/nataliia/koval/executor/Main.java
@@ -1,41 +1,31 @@
 package com.nataliia.koval.executor;
 
-import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
-
-import java.util.concurrent.CountDownLatch;
 import java.util.stream.IntStream;
+
 
 @Slf4j
 public class Main {
-    @SneakyThrows
+    private static final int TASK_COUNT = 9;
+    private static MyFixedThreadPool myFixedThreadPool = new MyFixedThreadPool(3);
+
+
     public static void main(String[] args) {
-        log.info("Creating MyFixedThreadPool instance in {}", Thread.currentThread().getName());
-        MyFixedThreadPool myFixedThreadPool = new MyFixedThreadPool(3);
+        log.info("Submitting {} tasks to MyFixedThreadPool", TASK_COUNT);
 
-        int taskCount = 9;
-        CountDownLatch latch = new CountDownLatch(taskCount);
-
-        log.info("Submitting {} tasks to MyFixedThreadPool", taskCount);
-        IntStream.range(1, taskCount + 1)
-                .mapToObj(num -> (Runnable) () -> {
-                    log.info("Task {} started by {}", num, Thread.currentThread().getName());
+        IntStream.range(1, TASK_COUNT + 1)
+                .mapToObj(number -> (Runnable) () -> {
+                    log.info("Task {} started by {}", number, Thread.currentThread().getName());
                     try {
                         Thread.sleep(100);
                     } catch (InterruptedException e) {
                         Thread.currentThread().interrupt();
-                        log.error("Task {} interrupted", num);
+                        log.error("Thread {} interrupted while running task {}", Thread.currentThread().getName(), number);
                     }
-                    log.info("Task {} completed by {}", num, Thread.currentThread().getName());
-                    latch.countDown();
+                    log.info("Task {} completed by {}", number, Thread.currentThread().getName());
                 })
                 .forEach(myFixedThreadPool::execute);
 
-        latch.await();
-
-        log.info("All tasks completed. Shutting down MyFixedThreadPool.");
         myFixedThreadPool.shutdown();
-
-        log.info("MyFixedThreadPool is terminated: {}", myFixedThreadPool.isTerminated());
     }
 }
--- src/main/java/com/nataliia/koval/executor/MyFixedThreadPool.java
+++ src/main/java/com/nataliia/koval/executor/MyFixedThreadPool.java
@@ -2,26 +2,23 @@ package com.nataliia.koval.executor;
 
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.Queue;
 import java.util.concurrent.*;
-import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.stream.IntStream;
 
 
 @Slf4j
 public class MyFixedThreadPool implements ExecutorService {
-    private final AtomicBoolean isShutdown = new AtomicBoolean(false);
+    private volatile boolean isShutdown = false;
     private final Queue<Runnable> tasks = new LinkedBlockingQueue<>();
     private final List<Thread> threads = new CopyOnWriteArrayList<>();
 
 
     public MyFixedThreadPool(int threadsCount) {
         log.info("Initializing MyFixedThreadPool with {} threads", threadsCount);
-        IntStream.range(0, threadsCount)
-                .mapToObj(i -> new Thread(new TaskExecutor(tasks, isShutdown), "my-pool-" + (i + 1)))
+        IntStream.range(0, threadsCount).mapToObj(i -> new Thread(new TaskExecutor(tasks), "my-pool-" + (i + 1)))
                 .peek(threads::add)
                 .forEach(Thread::start);
     }
@@ -29,57 +26,28 @@ public class MyFixedThreadPool implements ExecutorService {
 
     @Override
     public void execute(Runnable task) {
-        if (isShutdown.get()) {
+        if (isShutdown) {
             throw new RejectedExecutionException("Cannot accept new tasks, the pool is shutting down");
         }
+        tasks.add(task);
+
         synchronized (tasks) {
-            tasks.add(task);
             tasks.notifyAll();
-            log.info("Task added to the queue");
         }
     }
 
     @Override
     public void shutdown() {
-        log.info("Shutting down MyFixedThreadPool");
-
-        if (isShutdown.compareAndSet(false, true)) {
-            for (Thread thread : threads) {
-                thread.interrupt();
-            }
-
-            for (Thread thread : threads) {
-                try {
-                    thread.join();
-                    log.info("Thread {} has shutdown", thread.getName());
-                } catch (InterruptedException e) {
-                    Thread.currentThread().interrupt();
-                    log.error("InterruptedException during shutdown: {}", e.getMessage());
-                }
-            }
+        synchronized (tasks) {
+            tasks.notifyAll();
         }
+        isShutdown = true;
     }
+
 
     @Override
     public boolean isShutdown() {
-        return isShutdown.get();
-    }
-
-
-    @Override
-    public List<Runnable> shutdownNow() {
-        log.info("Force shutting down MyFixedThreadPool");
-        if (!isShutdown.get()) {
-            shutdown();
-        }
-
-        List<Runnable> remainingTasks = new ArrayList<>();
-        synchronized (tasks) {
-            while (!tasks.isEmpty()) {
-                remainingTasks.add(tasks.poll());
-            }
-        }
-        return remainingTasks;
+        return isShutdown;
     }
 
 
@@ -88,6 +56,57 @@ public class MyFixedThreadPool implements ExecutorService {
         return threads.stream().noneMatch(Thread::isAlive);
     }
 
+
+
+    class TaskExecutor implements Runnable {
+        private final Queue<Runnable> tasks;
+
+        public TaskExecutor(Queue<Runnable> tasks) {
+            this.tasks = tasks;
+        }
+
+        @Override
+        public void run() {
+            try {
+                while (!isShutdown || !tasks.isEmpty()) {
+                    Runnable task;
+                    synchronized (tasks) {
+                        while (tasks.isEmpty()) {
+                            tasks.wait();
+                        }
+                        task = tasks.poll();
+                    }
+                    if (task != null) {
+                        task.run();
+                    }
+                }
+            } catch (InterruptedException e) {
+                log.error("Thread {} interrupted. Error: {}", Thread.currentThread().getName(), e.getMessage());
+            } catch (Exception e) {
+                log.error("Exception occurred in thread {}: Error: {}", Thread.currentThread().getName(), e.getMessage());
+            }
+        }
+    }
+
+
+    @Override
+    public List<Runnable> shutdownNow() {
+        log.info("Force shutting down MyFixedThreadPool");
+
+        isShutdown = true;
+
+        for (Thread thread : threads) {
+            thread.interrupt();
+        }
+
+        List<Runnable> remainingTasks = new CopyOnWriteArrayList<>();
+        synchronized (tasks) {
+            while (!tasks.isEmpty()) {
+                remainingTasks.add(tasks.poll());
+            }
+        }
+        return remainingTasks;
+    }
 
     @Override
     public boolean awaitTermination(long timeout, TimeUnit unit) throws InterruptedException {
@@ -109,78 +128,41 @@ public class MyFixedThreadPool implements ExecutorService {
     }
 
 
-    static class TaskExecutor implements Runnable {
+// ---------------------------- not done
 
-        private final Queue<Runnable> tasks;
-        private final AtomicBoolean isShutdown;
 
-        public TaskExecutor(Queue<Runnable> tasks, AtomicBoolean isShutdown) {
-            this.tasks = tasks;
-            this.isShutdown = isShutdown;
-        }
-
-        @Override
-        public void run() {
-            try {
-                while (!Thread.interrupted()) {
-                    Runnable task;
-                    synchronized (tasks) {
-                        while (tasks.isEmpty() && !isShutdown.get()) {
-                            tasks.wait();
-                        }
-                        task = tasks.poll();
-                    }
-                    if (task != null) {
-                        log.info("{} is running the task", Thread.currentThread().getName());
-                        task.run();
-                    }
-                }
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
-                log.error("Thread {} interrupted", Thread.currentThread().getName());
-            } catch (Exception e) {
-                log.error("Exception occurred in thread {}: Error: {}", Thread.currentThread().getName(), e.getMessage());
-            }
-
-        }
+    @Override
+    public <T> Future<T> submit(Callable<T> task) {
+        return null;
     }
 
-
-        // ---------------------------- not done yet
-
-
-        @Override
-        public <T> Future<T> submit(Callable<T> task) {
-            return null;
-        }
-
-        @Override
-        public <T> Future<T> submit(Runnable task, T result) {
-            return null;
-        }
-
-        @Override
-        public Future<?> submit(Runnable task) {
-            return null;
-        }
-
-        @Override
-        public <T> List<Future<T>> invokeAll(Collection<? extends Callable<T>> tasks) throws InterruptedException {
-            return List.of();
-        }
-
-        @Override
-        public <T> List<Future<T>> invokeAll(Collection<? extends Callable<T>> tasks, long timeout, TimeUnit unit) throws InterruptedException {
-            return List.of();
-        }
-
-        @Override
-        public <T> T invokeAny(Collection<? extends Callable<T>> tasks) throws InterruptedException, ExecutionException {
-            return null;
-        }
-
-        @Override
-        public <T> T invokeAny(Collection<? extends Callable<T>> tasks, long timeout, TimeUnit unit) throws InterruptedException, ExecutionException, TimeoutException {
-            return null;
-        }
+    @Override
+    public <T> Future<T> submit(Runnable task, T result) {
+        return null;
     }
+
+    @Override
+    public Future<?> submit(Runnable task) {
+        return null;
+    }
+
+    @Override
+    public <T> List<Future<T>> invokeAll(Collection<? extends Callable<T>> tasks) throws InterruptedException {
+        return List.of();
+    }
+
+    @Override
+    public <T> List<Future<T>> invokeAll(Collection<? extends Callable<T>> tasks, long timeout, TimeUnit unit) throws InterruptedException {
+        return List.of();
+    }
+
+    @Override
+    public <T> T invokeAny(Collection<? extends Callable<T>> tasks) throws InterruptedException, ExecutionException {
+        return null;
+    }
+
+    @Override
+    public <T> T invokeAny(Collection<? extends Callable<T>> tasks, long timeout, TimeUnit unit) throws InterruptedException, ExecutionException, TimeoutException {
+        return null;
+    }
+}
